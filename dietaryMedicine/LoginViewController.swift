cbff9c9cacbae748fb9049269b836faa7cb85c7e
aroob6 <dlqhfcom@naver.com>
2022-05-16 12:02:39 +0900

    초기설정

---
+++ dietaryMedicine/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dietaryMedicine
+//
+//  Created by 이보라 on 2022/05/15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        loginButton.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
+    }
+
+    @objc func loginAction() {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let main = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as! MainTabBarController
+        main.modalPresentationStyle = .fullScreen
+        self.present(main, animated: false)
+    }
+}
+
